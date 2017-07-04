f5714d307fe0b1672c408cbdabcbe1f00b251440
Marnik Bercx <mbercx@gmail.com>
2017-07-04 00:34:24 -0700

    Fixed process_output.py so it actually writes the file to the right folder.

--- cage/scripts/process_output.py
+++ cage/scripts/process_output.py
@@ -1,14 +1,16 @@
 # Encoding: utf-8
 
 import sys
+import os
 
 import pymatgen.io.nwchem as nwchem
 
 output_file = sys.argv[1]
+dir_name = os.path.dirname(output_file)
 
 try:
     output = nwchem.NwOutput(output_file)
 except:
     raise IOError('Could not find proper nwchem output file.')
 
-output.to_file('data.json')
+output.to_file(os.path.join(dir_name, 'data.json'))
