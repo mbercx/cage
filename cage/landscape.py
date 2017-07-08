10011dfecfbb81bb155575367e11dd7c0a004e2b
mbercx <mbercx@gmail.com>
2017-07-07 17:57:32 -0700

    Made the rotation of rotation_matrix clockwise. Made the rotation script easier to read.

--- cage/landscape.py
+++ cage/landscape.py
@@ -104,9 +104,11 @@ class Landscape(MSONable):
 
         # Find the rotation matrices
         rotation_matrices = []
+        total_angle = np.linalg.norm(axis)
+        npoints = int(total_angle/math.pi*density)
 
-        for i in range(int(density)):
-            angle = np.linalg.norm(axis)*(i+1)/int(density)
+        for i in range(npoints):
+            angle = (i+1)/npoints*total_angle
             rotation_matrices.append(rotation_matrix(axis, angle))
 
         # Add all the points TODO This might be done more quickly
@@ -427,13 +429,13 @@ def angle_between(v1, v2):
 
 def rotation_matrix(axis, theta):
     """
-    Return the rotation matrix associated with counterclockwise rotation about
+    Return the rotation matrix associated with clockwise rotation about
     the given axis by theta radians.
     """
     axis = np.asarray(axis)
     axis = axis/math.sqrt(np.dot(axis, axis))
     a = math.cos(theta/2.0)
-    b, c, d = -axis*math.sin(theta/2.0)
+    b, c, d = axis*math.sin(theta/2.0)
     aa, bb, cc, dd = a*a, b*b, c*c, d*d
     bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
     return np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
