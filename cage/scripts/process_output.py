252719b4ab676f5981bbbfbf07ec0e544f72139f
mbercx <mbercx@gmail.com>
2017-07-14 10:12:20 -0700

    Now skips a directory if it already has a data.json file.

--- cage/scripts/process_output.py
+++ cage/scripts/process_output.py
@@ -8,9 +8,10 @@ import pymatgen.io.nwchem as nwchem
 output_file = sys.argv[1]
 dir_name = os.path.dirname(output_file)
 
-try:
-    output = nwchem.NwOutput(output_file)
-except:
-    raise IOError('Could not find proper nwchem output file.')
+if 'data.json' not in os.listdir(dir_name):
+    try:
+        output = nwchem.NwOutput(output_file)
+    except:
+        raise IOError('Could not find proper nwchem output file.')
 
-output.to_file(os.path.join(dir_name, 'data.json'))
+    output.to_file(os.path.join(dir_name, 'data.json'))
